8d4c9986430bfc24c3390c7e5eaa70c9a185cb46
Luis Felipe Maciel Pierro <luisf.pierro@gmail.com>
2020-11-25 16:56:36 -0300

    arrumando home

--- src/main/java/br/com/gama/projetofinal/controller/HomeController.java
+++ src/main/java/br/com/gama/projetofinal/controller/HomeController.java
@@ -1,8 +1,11 @@
-package br.com.agendamento.controller;
+package br.com.gama.projetofinal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
