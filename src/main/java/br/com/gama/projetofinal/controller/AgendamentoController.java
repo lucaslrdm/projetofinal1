fcf25bd103d08c6f7cfbd5da15361f9f710033c9
Luis Felipe Maciel Pierro <luisf.pierro@gmail.com>
2020-11-27 13:24:32 -0300

    volta nomral

--- src/main/java/br/com/gama/projetofinal/controller/AgendamentoController.java
+++ src/main/java/br/com/gama/projetofinal/controller/AgendamentoController.java
@@ -98,7 +98,7 @@ public class AgendamentoController {
 	
 	@GetMapping("/agendamentos")
 	public ResponseEntity<List<Agendamento>> getAll(){
-		List<Agendamento> lista = (List<Agendamento>) dao.findAllOrderByDataagendamentoAscHoraagendamentoAsc();
+		List<Agendamento> lista = (List<Agendamento>) dao.findAll();
 		if(lista.size()==0) return ResponseEntity.status(404).build();
 		return ResponseEntity.ok(lista);
 	}
--- src/main/java/br/com/gama/projetofinal/dao/AgendamentoDAO.java
+++ src/main/java/br/com/gama/projetofinal/dao/AgendamentoDAO.java
@@ -25,6 +25,6 @@ public interface AgendamentoDAO extends CrudRepository<Agendamento, Integer>{
 	
 	public List<Agendamento> findByHoraagendamentoAndDataagendamentoAndAgenciaId(String horaagendamento,Date dataagendamento,int id);
 	
-	public List<Agendamento> findAllOrderByDataagendamentoAscHoraagendamentoAsc();
+	public List<Agendamento> findByOrderByDataagendamentoAscHoraagendamentoAsc();
 
 }
